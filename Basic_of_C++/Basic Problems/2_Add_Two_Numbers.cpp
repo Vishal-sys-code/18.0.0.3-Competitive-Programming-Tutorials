102c79f53f8caf8b7012f1ded2092ed1de2a84f1
Vishal Pandey <bishalpandey2001@gmail.com>
2023-02-18 12:15:04 +0530

    update_1

---
+++ Basic_of_C++/Basic Problems/2_Add_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Input_Output() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+}
+
+int main() {
+	Input_Output();
+	cout << "HELLO WORLD" << endl;
+	return 0;
+}
